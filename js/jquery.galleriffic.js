312885ee93931a117f5ddc2e6cacea3704301ffc
Jay Hayes <ur@iamvery.com>
2011-07-04 12:17:58 -0500

    Version 2.0.2 bump; Added self credit

--- js/jquery.galleriffic.js
+++ js/jquery.galleriffic.js
@@ -6,6 +6,8 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *
  * Much thanks to primary contributer Ponticlaro (http://www.ponticlaro.com)
+ *
+ * Modifed by Jay Hayes (http://iamvery.com)
  */
 ;(function($) {
 	// Globally keep track of all images by their unique hash.  Each item is an image data object.
@@ -14,7 +16,7 @@
 
 	// Galleriffic static class
 	$.galleriffic = {
-		version: '2.0.1',
+		version: '2.0.2',
 
 		// Strips invalid characters and any leading # characters
 		normalizeHash: function(hash) {
